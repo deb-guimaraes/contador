d15d0a509741fe4ba03fa641448b207cb3c77dd9
Débora Guimarães <debb.oliveira2@gmail.com>
2025-02-23 17:54:10 -0300

    ver 01

---
+++ README.md
@@ -0,0 +1,9 @@
+# Contador
+
+Este projeto é um contador numérico simples. Para ele, foi utilizado HTML, CSS e JavaScript.
+
+Projeto realizado em 06 de Outubro de 2024.
+
+## O projeto
+
+<img src="contador/assets/img.png" />
---
+++ script.js
@@ -0,0 +1,23 @@
+const dec = document.querySelector(".decrease");
+const reset = document.querySelector(".reset");
+const inc = document.querySelector(".increase");
+
+let valor = document.querySelector("span");
+
+let num = 0;
+valor.innerHTML = num;
+
+inc.addEventListener("click", () => {
+    num++;
+    valor.innerHTML = num
+})
+
+dec.addEventListener("click", () => {
+    num--
+    valor.innerHTML = num
+})
+
+reset.addEventListener("click", () => {
+    num = 0
+    valor.innerHTML = num
+})
